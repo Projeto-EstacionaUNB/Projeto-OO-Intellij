081256298a6c8bd34c46b8427e70bb59901ad0fe
oo7gabriel <gabrielfsacademico@gmail.com>
2022-09-05 00:47:17 -0300

    Colocando os códigos do eclipse

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,4 +1,62 @@
 package view;
 
+import java.util.List;
+import java.util.Scanner;
+
+import model.*;
+
+
 public class Main {
+
+    static List<Estacionamento> estacionamento;
+    public static void main(String[] args) {
+        operacoes();
+    }
+
+    public static void operacoes() {
+        Scanner escanear = new Scanner(System.in);
+        System.out.println("------------------------------------------------------");
+        System.out.println("-------------Bem vindos ao Sistema de Estacionamento ---------------");
+        System.out.println("------------------------------------------------------");
+        System.out.println("***** Selecione uma ação que deseja realizar *****");
+        System.out.println("------------------------------------------------------");
+        System.out.println("|   Opção 1 - Cadastrar Estacionamento   |");
+        System.out.println("|   Opção 2 - Entrar em um estacioanamento     |");
+        System.out.println("|   Opção 3 - Cadastrar Eventos         |");
+        System.out.println("|   Opção 4 - Listar Estacionamentos    |");
+        System.out.println("|   Opção 6 - Sair          |");
+
+        int operacao = escanear.nextInt();
+
+        switch (operacao){
+
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+
+            case 5:
+                System.out.println("Obrigado por usar o sistema :) ");
+                System.exit(0);
+                break;
+
+            default:
+                System.out.println("Opção inválida");
+                operacoes();
+                break;
+        }
+        escanear.close();
+    }
+
 }
