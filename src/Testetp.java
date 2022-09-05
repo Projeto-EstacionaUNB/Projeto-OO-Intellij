b92920ba4d04659dc6f75ca9bc473ecaae129720
Danilo <danilocarvalhoantunes@gmail.com>
2022-09-05 15:45:15 -0300

    Classe de LocalDateTime , ainda para ajustar.

--- src/Testetp.java
+++ src/Testetp.java
@@ -16,12 +16,42 @@ public class Testetp {
         LocalDateTime date1 = LocalDateTime.parse(entrada);
         LocalDateTime date2 = LocalDateTime.parse(saida);
         Duration duracao = Duration.between(date1, date2);
-        long dif = duracao.toHours();
-        long diff = duracao.toMinutes();
-        diff = diff%60 ;
-        System.out.println("horas:" + dif + "  minutos:"+ diff + " " +duracao);
+        long Horas = duracao.toHours();
+        long Minutos= duracao.toMinutes();
+        Minutos = Minutos%60 ;
+        System.out.println("horas:" + Horas + "  minutos:"+ Minutos + " " +duracao);
         //2022-09-05T10:45
         //2022-09-06T11:50
 
+        System.out.println("Digite a fração de tempo do estacionamento \n Escreva em minutos");
+        String fracao = esc.next();
+        double Fracao = Double.parseDouble(fracao);
+        double numerofração = Math.ceil(Minutos/Fracao);
+        System.out.println(numerofração);
+
+        System.out.println("Digite o valor da fração do estacionamento ");
+        String valorFracao = esc.next();
+        double ValorFracao = Double.parseDouble(valorFracao);
+
+        System.out.println("Digite o valor do desconto \n Escreva em decial \n Exemplo : 20 porcento de desconto é 0.20");
+        String desconto = esc.next();
+        double Desconto = Double.parseDouble(desconto);
+        Desconto = 1.00 - Desconto;
+        double numeroFracoes = Horas * (60/Fracao);
+        System.out.println("Numero de frações dentro das horas : " + numeroFracoes);
+        double valorHora = numeroFracoes * ValorFracao;
+        System.out.println("valor das horas sem desconto : " + valorHora);
+        double valorHoras = valorHora * Desconto;
+        System.out.println("valor das horas com desconto: " + valorHoras);
+
+        double valorMinutos = numerofração * ValorFracao;
+        System.out.println("valor dos minutos: " + valorMinutos);
+
+        double custo = valorHoras + valorMinutos;
+        System.out.println("O custo foi de : " + custo);
+
+
+        //2022-09-05T12:07
+        //2022-09-05T14:40
         }
 }
